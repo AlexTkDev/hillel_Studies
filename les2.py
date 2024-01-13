ed9b5da7988add33376dcbefe4aeed370fd24939
alex <a8696tk@gmail.com>
2024-01-13 14:53:45 +0200

    homework done

--- les2.py
+++ les2.py
@@ -1,2 +1,19 @@
-name = input('What is your name?')
-print('Hello',name + '!')
+user_number = int(input("Enter a four-digit number:"))
+
+fourth_number = user_number % 10
+user_number //= 10
+third_number = user_number % 10
+user_number //= 10
+second_number = user_number % 10
+user_number //= 10
+first_number = user_number
+
+print(first_number)
+print(second_number)
+print(third_number)
+print(fourth_number)
+
+# Людяний вариант, де не потрибноо ломати мозоок та ссивіти )))
+user_number = input("Enter a four-digit number:")
+for num in user_number:
+    print(int(num))
