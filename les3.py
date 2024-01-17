b3a6ed37b70a59178cc52bab89bbaf5f1b41ddb6
alex <a8696tk@gmail.com>
2024-01-17 12:40:10 +0200

    finished the first task

---
+++ les3.py
@@ -0,0 +1,47 @@
+#                                  ----------- EX 1 ------------
+
+####################### Calculator using if ######################
+# num_1 =int(input('Enter a number 1: '))
+# num_2 =int(input('Enter a number 2: '))
+# action =input('Enter action: ')
+#
+# if action == '/':
+#     if num_2 != 0:
+#         result = num_1 / num_2
+#     else:
+#         result = 0
+# elif action == '+':
+#     result = num_1 + num_2
+# elif action == '-':
+#     result = num_1 - num_2
+# else:
+#     result = num_1 * num_2
+#
+# print(f"The result of = {result}")
+
+
+############## Using the method match ###############
+# num_1 = int(input('Enter a number 1: '))
+# num_2 = int(input('Enter a number 2: '))
+# action = input('Enter action: ')
+# result = None
+#
+# if action == '+' or action == '-' or action == '+' or action == '/' or action == '*':
+#     match action:
+#         case '/':
+#             if num_2 != 0:
+#                 result = num_1 / num_2
+#             else:
+#                 result = 0
+#         case '+':
+#             result = num_1 + num_2
+#         case '-':
+#             result = num_1 - num_2
+#         case '*':
+#             result = num_1 * num_2
+# else:
+#     result = 'Invalid value, enter +, -, *, / '
+# print(f'The result is = {result}')
+
+#                                  ----------- EX 2 ------------
+
