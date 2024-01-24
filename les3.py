22bf36748f58e4e185d9c1331242969c5cd5188a
Aleksandr <a8696tk@gmail.com>
2024-01-24 12:18:55 +0200

    fix

--- les3.py
+++ les3.py
@@ -22,28 +22,28 @@
 
 
 ############## Using the method match ###############
-
-num_1 = int(input('Enter a number 1: '))
-num_2 = int(input('Enter a number 2: '))
-action = input('Enter action: ')
-
-if action == '+' or action == '-' or action == '+' or action == '/' or action == '*':
-    match action:
-        case '/':
-            if num_2 != 0:
-                result = num_1 / num_2
-            else:
-                result = 0
-        case '+':
-            result = num_1 + num_2
-        case '-':
-            result = num_1 - num_2
-        case '*':
-            result = num_1 * num_2
-else:
-    result = 'Invalid value, enter +, -, *, / '
-
-print(f'The result is = {result}')
+#
+# num_1 = int(input('Enter a number 1: '))
+# num_2 = int(input('Enter a number 2: '))
+# action = input('Enter action: ')
+#
+# if action == '+' or action == '-' or action == '+' or action == '/' or action == '*':
+#     match action:
+#         case '/':
+#             if num_2 != 0:
+#                 result = num_1 / num_2
+#             else:
+#                 result = 0
+#         case '+':
+#             result = num_1 + num_2
+#         case '-':
+#             result = num_1 - num_2
+#         case '*':
+#             result = num_1 * num_2
+# else:
+#     result = 'Invalid value, enter +, -, *, / '
+#
+# print(f'The result is = {result}')
 
 #                                  ----------- EX 2 ------------
 
@@ -58,3 +58,12 @@ print(f'The result is = {result}')
 #     my_list[0] = new_list
 #     result = my_list
 # print(result)
+
+#################### next method
+
+original_list = [1, 2, 3, 4, 5]
+
+if len(original_list) >1:
+    original_list.insert(0, original_list.pop())
+
+print(original_list)
