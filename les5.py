9c0750f6bcbb870a0833091d081dd101caf9fd54
alex <a8696tk@gmail.com>
2024-01-31 13:34:07 +0200

    add other option ex2

--- les5.py
+++ les5.py
@@ -36,11 +36,11 @@
 
 
 #                           ------------- EX 2 ----------
-from keyword import kwlist
-
-result = True
-
-value_name = "_"
+# option 1
+#
+# from keyword import kwlist
+#
+# value_name = "_"
 # value_name = "x"
 # value_name = "get_value"
 # value_name = "get value"
@@ -51,24 +51,76 @@ value_name = "_"
 # value_name = "getValue"
 # value_name = "3m"
 # value_name = "m3"
+#
+# result = True
+#
+# literal = '''!"#$%&'()*+,-./:;<=>?@[]{}\\^`|~'''
+# kw_list = kwlist
+#
+# if not value_name[0].isdigit() and not value_name[0].isupper():
+#     if not value_name.isdigit() and not value_name.isupper():
+#         if value_name.find(" ") == -1:
+#             if value_name not in kw_list:
+#                 for letter in value_name:
+#                     if letter not in literal and not letter.isupper():
+#                         result = True
+#
+#                     else:
+#                         result = False
+#                         break
+#         else:
+#             result = False
+# else:
+#     result = False
+#
+# print(result)
 
-literal = '''!"#$%&'()*+,-./:;<=>?@[]{}\\^`|~'''
-kw_list = kwlist
+# option 2
+#
+# import keyword, string
+#
+# value_name = "_"
+# value_name = "x"
+# value_name = "get_value"
+# value_name = "get value"
+# value_name = "get!value"
+# value_name = "some_super_puper_value"
+# value_name = "Get_value"
+# value_name = "get_Value"
+# value_name = "getValue"
+# value_name = "3m"
+# value_name = "m3"
+#
+#
+# value_name = input("Enter value ")
+# result = True
+#
+# if value_name[0].isdigit():
+#     result = False
+# if value_name in keyword.kwlist:
+#     result = False
+#
+# for letter in value_name:
+#     if not result:
+#         break
+#     if letter.isupper():
+#         result = False
+#         break
+#     if letter == "_":
+#         continue
+#     if letter in string.punctuation or letter.isspace():
+#         result = False
+#         break
+#
+# print(result)
 
-if not value_name[0].isdigit() and not value_name[0].isupper():
-    if not value_name.isdigit() and not value_name.isupper():
-        if value_name.find(" ") == -1:
-            if value_name not in kw_list:
-                for letter in value_name:
-                    if letter not in literal and not letter.isupper():
-                        result = True
+# option 3 - The best
+#
+# value_name = input("Enter value ")
+# result = False
+# if value_name.identifier():
+#     if value_name == "_" or value_name.islower():
+#         result = True
+#
+# print(result)
 
-                    else:
-                        result = False
-                        break
-        else:
-            result = False
-else:
-    result = False
-
-print(result)
