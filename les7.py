a2cca213290c11615219f604efd71de7d24bb21f
alex <a8696tk@gmail.com>
2024-02-01 12:37:17 +0200

    added option 2 in ex 2

--- les7.py
+++ les7.py
@@ -1,24 +1,34 @@
 #                           ------------ EX 1 --------------
 #
-def say_hi(name, age):
-    result = f"Hi. My name is {name} and I'm {age} years old"
-    return result
-
-
-assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
-assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
-print('ОК')
+# def say_hi(name, age):
+#     result = f"Hi. My name is {name} and I'm {age} years old"
+#     return result
+#
+#
+# assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+# assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+# print('ОК')
 
 
 #                           ------------ EX 2 --------------
 
-def correct_sentence(text):
-    if text[0].islower():
-        new_text = text[0].upper() + text[1:]
-    else:
-        new_text = text
+# # option 1
+# def correct_sentence(text):
+#     if text[0].islower():
+#         new_text = text[0].upper() + text[1:]
+#     else:
+#         new_text = text
+#
+#     if text[-1] != ".":
+#         new_text += "."
+#
+#     return new_text
 
-    if text[-1] != ".":
+#  option 2
+def correct_sentence(text):
+    new_text = text[0].capitalize() + text[1:]
+
+    if not text.endswith("."):
         new_text += "."
 
     return new_text
