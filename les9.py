afedd8d064cfff3aed845d92f166fc0ab2ba1263
alex <a8696tk@gmail.com>
2024-02-08 12:26:40 +0200

    finished second exercise

--- les9.py
+++ les9.py
@@ -13,3 +13,25 @@ def popular_words(text, words):
 assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
                      ['i', 'was', 'three', 'near']) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}, 'Test1'
 print('OK')
+
+
+#                     ------------------------ EX 2 -----------------------
+
+from typing import Union
+
+
+def difference(*args) -> Union[int, float]:
+    if len(args) == 0:
+        return 0
+
+    min_elem = min(args)
+    max_elem = max(args)
+    result = (max_elem - min_elem)
+    return round(result, 2)
+
+
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
+print('OK')
