f5939ea2fde00af34b9ae8ad524f2b52b68c651c
alex <a8696tk@gmail.com>
2024-02-12 10:48:11 +0200

    add second option, solution through the ternary operator

--- les10.py
+++ les10.py
@@ -27,8 +27,11 @@ print('OK')
 
 def is_even(digit):
     """ Перевірка чи є парним число """
-    if digit % 2 == 0: return True
-    else: return False
+    # if digit % 2 == 0: return True
+    # else: return False
+
+    # or
+    return True if digit % 2 == 0 else False
 
 assert is_even(2) == True, 'Test1'
 assert is_even(5) == False, 'Test2'
