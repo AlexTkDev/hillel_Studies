8bccda55532482a1a7858a3ee1b03999e267306d
alex <a8696tk@gmail.com>
2024-02-12 11:24:15 +0200

    fix ex2

--- les10.py
+++ les10.py
@@ -27,11 +27,10 @@ print('OK')
 
 def is_even(digit):
     """ Перевірка чи є парним число """
-    # if digit % 2 == 0: return True
-    # else: return False
+    return digit % 2 == 0
 
     # or
-    return True if digit % 2 == 0 else False
+    # return True if digit % 2 == 0 else False
 
 assert is_even(2) == True, 'Test1'
 assert is_even(5) == False, 'Test2'
