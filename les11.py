02c84490c1bde8d3466c6afd909a73f32001b924
alex <a8696tk@gmail.com>
2024-02-14 15:59:35 +0200

    fix ex1

--- les11.py
+++ les11.py
@@ -24,7 +24,7 @@ assert isgenerator(gen) == True, 'Test0'
 assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
 assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
 assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
-print('Ok')
+print('Ok!')
 
 
 #                       ------------------  EX 2  ---------------------
@@ -37,4 +37,4 @@ def is_even(number):
 assert is_even(2494563894038 ** 2) == True, 'Test1'
 assert is_even(1056897 ** 2) == False, 'Test2'
 assert is_even(24945638940387 ** 3) == False, 'Test3'
-print('Ok')
+print('Ok!')
