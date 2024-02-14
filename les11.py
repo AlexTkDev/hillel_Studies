3cff45740a1096a12cbd2443519e3e73873caa9c
alex <a8696tk@gmail.com>
2024-02-14 15:48:02 +0200

    finished ex2

--- les11.py
+++ les11.py
@@ -26,3 +26,15 @@ assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
 assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
 print('Ok')
 
+
+#                       ------------------  EX 2  ---------------------
+
+
+def is_even(number):
+    return (number & 1) == 0
+
+
+assert is_even(2494563894038 ** 2) == True, 'Test1'
+assert is_even(1056897 ** 2) == False, 'Test2'
+assert is_even(24945638940387 ** 3) == False, 'Test3'
+print('Ok')
