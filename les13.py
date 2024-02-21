2263785e2cb45ee004b311d8c9ede7715363ada6
alex <a8696tk@gmail.com>
2024-02-21 14:39:06 +0200

    finished the second task

--- les13.py
+++ les13.py
@@ -1,4 +1,4 @@
-# # -------------------------- EX 1 -------------------------
+# -------------------------- EX 1 -------------------------
 
 class Human:
 
@@ -42,6 +42,7 @@ class Group:
                 return student
 
 
+
     def __str__(self):
         all_students = ''
         for student in self.group:
@@ -66,53 +67,60 @@ gr.delete_student('Taylor')  # No error!
 
 
 # -------------------------- EX 2 -------------------------
-#
-# class Counter:
-#
-#    def __init__(self, current=1, min_value=0, max_value=10):
-#        self.current = current
-#        self.min_value = min_value
-#        self.max_value = max_value
-#
-#    def set_current(self, start):
-#        self.current = start
-#
-#
-#    def set_max(self, max_max):
-#         pass
-#
-#    def set_min(self, min_min):
-#        pass
-#
-#    def step_up(self):
-#        pass
-#
-#    def step_down(self):
-#        pass
-#
-#    def get_current(self):
-#        return self.current
-#
-# counter = Counter()
-# counter.set_current(7)
-# counter.step_up()
-# counter.step_up()
-# counter.step_up()
-# assert counter.get_current() == 10, 'Test1'
-# try:
-#     counter.step_up()  # ValueError
-# except ValueError as e:
-#     print(e) # Досягнуто максимуму
-# assert counter.get_current() == 10, 'Test2'
-#
-#
-# counter.set_min(7)
-# counter.step_down()
-# counter.step_down()
-# counter.step_down()
-# assert counter.get_current() == 7, 'Test3'
-# try:
-#     counter.step_down()  # ValueError
-# except ValueError as e:
-#     print(e) # Досягнуто мінімуму
-# assert counter.get_current() == 7, 'Test4'
+
+class Counter:
+
+    def __init__(self, current=1, min_value=0, max_value=10):
+        self.current = current
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def set_current(self, start):
+        self.current = start
+
+    def set_max(self, max_max):
+        self.max_value = max_max
+
+    def set_min(self, min_min):
+        self.min_value = min_min
+
+    def step_up(self):
+        if self.current == self.max_value:
+            raise ValueError("Reached maximum value = ", self.max_value)
+        else:
+            self.current += 1
+        return self.current
+
+    def step_down(self):
+        if self.current == self.min_value:
+            raise ValueError("Reached minimum value = ", self.min_value)
+        else:
+            self.current -= 1
+        return self.min_value
+
+    def get_current(self):
+        return self.current
+
+
+counter = Counter()
+counter.set_current(7)
+counter.step_up()
+counter.step_up()
+counter.step_up()
+assert counter.get_current() == 10, 'Test1'
+try:
+    counter.step_up()  # ValueError
+except ValueError as e:
+    print(e)  # Досягнуто максимуму
+assert counter.get_current() == 10, 'Test2'
+
+counter.set_min(7)
+counter.step_down()
+counter.step_down()
+counter.step_down()
+assert counter.get_current() == 7, 'Test3'
+try:
+    counter.step_down()  # ValueError
+except ValueError as e:
+    print(e)  # Досягнуто мінімуму
+assert counter.get_current() == 7, 'Test4'
