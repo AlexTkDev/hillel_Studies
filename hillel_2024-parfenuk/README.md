22ac28d2484b905b9c72221d1bfa8d3ee6460ae3
Aleksandr <105745366+AlexTkDev@users.noreply.github.com>
2024-06-14 13:44:36 +0300

    Add files via upload

---
+++ hillel_2024-parfenuk/README.md
@@ -0,0 +1,10 @@
+# Concetps
+
+- [x] decorators(15%)
+- [x] GIT (20%)
+- [x] LEGB (local, enclosure, global, built-in)
+- [x] MRO
+- [x] Context managers
+- [x] Files
+- [x] `__new__`, `__init__`
+- [ ] `@property`
---
+++ hillel_2024-parfenuk/lesson_02/oop.py
@@ -0,0 +1,23 @@
+class Person:
+    def __init__(self, name: str, age: int) -> None:
+        self.name: str = name
+        self.age: int = age
+
+
+class Price:
+    def __init__(self, currency: str, value: int) -> None:
+        self.currency: str = currency
+        self.value: int = value
+
+    # TODO: Complete this class
+
+
+# Another line
+
+
+# phone = 1000 USD
+# food = 100 UAH
+# total = phone + food
+
+# 1000 + (100 / 39) = 1000 + 2.56410256 USD
+# 1000 * 39 + 100 = ...
